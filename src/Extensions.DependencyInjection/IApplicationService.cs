5ffef9a88c8cbcd1c30ce5aac6b56e72151ac554
xm <xmid1001@gmail.com>
2020-06-02 10:57:47 +0900

    add feature Extensions.DependencyInjection

---
+++ src/Extensions.DependencyInjection/IApplicationService.cs
@@ -0,0 +1,9 @@
+namespace AspNetCoreNuxt.Extensions.DependencyInjection
+{
+    /// <summary>
+    /// アプリケーションサービスを表します。
+    /// </summary>
+    public interface IApplicationService
+    {
+    }
+}
