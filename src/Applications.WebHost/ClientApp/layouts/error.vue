5677221a45e287135c07e842c01b3ec1cf975e30
xm <xmid1001@gmail.com>
2020-06-09 00:00:18 +0900

    add error.vue

---
+++ src/Applications.WebHost/ClientApp/layouts/error.vue
@@ -0,0 +1,37 @@
+<script>
+export default {
+  props: {
+    error: {
+      default: null,
+      type: Object,
+    },
+  },
+};
+</script>
+
+<template>
+  <v-content>
+    <v-container fluid>
+      <v-row justify="center">
+        <v-col class="text-center">
+          <h1 class="display-4 font-weight-medium my-5 primary--text">
+            {{ error.statusCode }}
+          </h1>
+          <h4 class="title">
+            {{ error.message }}
+          </h4>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-content>
+</template>
+
+<style lang="scss" scoped>
+.v-content ::v-deep {
+  height: 100%;
+  left: 0;
+  position: absolute;
+  top: 0;
+  width: 100%;
+}
+</style>
