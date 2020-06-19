6156ff78b013cb172571ecfbdad42d7a43928fd0
xm <xmid1001@gmail.com>
2020-06-19 11:30:54 +0900

    update plugin prototype

--- src/Applications.WebHost/ClientApp/plugins/prototype.js
+++ src/Applications.WebHost/ClientApp/plugins/prototype.js
@@ -2,6 +2,11 @@ Array.create = function (iterable) {
   return Array.from(iterable || []);
 };
 
-Array.prototype.ofIntegers = function () {
+Array.prototype.ofNumbers = function () {
   return this.map(x => Number(x?.toString().trim() || undefined)).filter(x => Number.isInteger(x));
+};
+
+Number.is32bitInteger = function (val) {
+  const value = +val;
+  return -2147483648 <= value && value <= 2147483647 && /^[+-]?[0-9]+$/.test(value);
 };
