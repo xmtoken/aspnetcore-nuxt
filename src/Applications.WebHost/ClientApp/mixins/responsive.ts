c97a03820f4f53fc7e4b8ed7593aded79c0e6aad
xm <xmid1001@gmail.com>
2020-08-06 12:10:49 +0900

    bugfix responsive with fontfaceobserver

--- src/Applications.WebHost/ClientApp/mixins/responsive.ts
+++ src/Applications.WebHost/ClientApp/mixins/responsive.ts
@@ -5,6 +5,16 @@ import responsive from '~/extensions/responsive';
 export default Vue.extend({
   created(): void {
     const font = new FontFaceObserver('Noto Sans JP');
-    font.load().then(() => responsive());
+    const fontTestString = [
+      // ‰p‘å•¶Žš
+      ...[...Array(26)].map((_, i) => String.fromCharCode(i + 65)),
+      // ‰p¬•¶Žš
+      ...[...Array(26)].map((_, i) => String.fromCharCode(i + 97)),
+      // •½‰¼–¼
+      ...[...Array(86)].map((_, i) => String.fromCharCode(i + 12353)),
+      // •Ð‰¼–¼
+      ...[...Array(101)].map((_, i) => String.fromCharCode(i + 12443)),
+    ].join('');
+    font.load(fontTestString).then(() => responsive());
   },
 });
