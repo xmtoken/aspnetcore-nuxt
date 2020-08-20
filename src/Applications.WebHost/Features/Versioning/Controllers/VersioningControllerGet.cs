3696d813b4be7d1b5a1261b3bf3b4d6b3d2da901
xm <xmid1001@gmail.com>
2020-08-21 00:18:37 +0900

    fix CA1822 on Versioning API

--- src/Applications.WebHost/Features/Versioning/Controllers/VersioningControllerGet.cs
+++ src/Applications.WebHost/Features/Versioning/Controllers/VersioningControllerGet.cs
@@ -1,6 +1,7 @@
 using AspNetCoreNuxt.Applications.WebHost.Core.Mvc;
 using AspNetCoreNuxt.Applications.WebHost.Features.Versioning.Models;
 using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics.CodeAnalysis;
 
 namespace AspNetCoreNuxt.Applications.WebHost.Features.Versioning.Controllers
 {
@@ -12,6 +13,7 @@ namespace AspNetCoreNuxt.Applications.WebHost.Features.Versioning.Controllers
         /// <returns><see cref="AppVersion"/> オブジェクト。</returns>
         [ApiVersionNeutral]
         [HttpGet]
+        [SuppressMessage("Performance", "CA1822:メンバーを static に設定します")]
         public AppVersion Get()
         {
             return new AppVersion()
