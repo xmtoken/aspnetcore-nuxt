a7293d20bf40f4e7d57a5cff648a73fb57859231
xm <xmid1001@gmail.com>
2020-08-26 21:44:53 +0900

    suppress SA1210 on Domains.Data.Design

--- src/Domains.Data.Design/GlobalSuppressions.cs
+++ src/Domains.Data.Design/GlobalSuppressions.cs
@@ -2,4 +2,5 @@ using System.Diagnostics.CodeAnalysis;
 
 [assembly: SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1600:Elements should be documented")]
 [assembly: SuppressMessage("StyleCop.CSharp.NamingRules", "SA1300:Element should begin with upper-case letter")]
+[assembly: SuppressMessage("StyleCop.CSharp.OrderingRules", "SA1210:Using directives should be ordered alphabetically by namespace")]
 [assembly: SuppressMessage("StyleCop.CSharp.ReadabilityRules", "SA1413:Use trailing comma in multi-line initializers")]
