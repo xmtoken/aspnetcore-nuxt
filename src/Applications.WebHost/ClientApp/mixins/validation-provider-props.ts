c49506509cbcbf60471ec98f438157368d1c1785
xm <xmid1001@gmail.com>
2020-08-31 22:39:09 +0900

    add disabled prop to validationProviderProps mixin

--- src/Applications.WebHost/ClientApp/mixins/validation-provider-props.ts
+++ src/Applications.WebHost/ClientApp/mixins/validation-provider-props.ts
@@ -2,6 +2,7 @@ import Vue, { PropType } from 'vue';
 
 type ValidationProviderProperties = {
   customMessages: object;
+  disabled: boolean;
   name: string;
   rules: string | object | null;
   slim: boolean;
@@ -17,6 +18,10 @@ export default Vue.extend({
     veeCustomMessages: {
       default: undefined,
       type: Object as PropType<object>,
+    },
+    veeDisabled: {
+      default: false,
+      type: Boolean,
     },
     veeDisabledRules: {
       default: false,
@@ -39,6 +44,7 @@ export default Vue.extend({
     veeProviderProps(): ValidationProviderProperties {
       return {
         customMessages: this.veeCustomMessages,
+        disabled: this.veeDisabled,
         name: this.veeName ? this.veeName : this.label,
         rules: this.veeDisabledRules ? null : this.veeRules,
         slim: true,
