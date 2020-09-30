4ca1dd5c390fec27fa0037bcf1bec310cc857b9b
xm <xmid1001@gmail.com>
2020-09-30 20:54:02 +0900

    add autocomplete component

---
+++ src/Applications.WebHost/ClientApp/components/AppAutocomplete.vue
@@ -0,0 +1,37 @@
+<script lang="ts">
+import { ValidationProvider } from 'vee-validate';
+import { PropType } from 'vue';
+import mixins from '~/extensions/mixins';
+import iconTabIndexable from '~/mixins/icon-tab-indexable';
+import requiredMarkable from '~/mixins/required-markable';
+import slotable from '~/mixins/slotable';
+import validationProviderProps from '~/mixins/validation-provider-props';
+
+export default mixins(iconTabIndexable, requiredMarkable, slotable, validationProviderProps).extend({
+  components: {
+    ValidationProvider,
+  },
+  inheritAttrs: false,
+  props: {
+    menuProps: {
+      default(): object {
+        return {
+          offsetY: true,
+        };
+      },
+      type: [Object, String] as PropType<string | object>,
+    },
+  },
+});
+</script>
+
+<template>
+  <validation-provider v-slot="{ errors, required }" v-bind="veeProviderProps">
+    <v-autocomplete v-bind="$attrs" :class="{ required, 'required-marker': required && !disabledRequiredMarker }" :disabled="disabled" :error-messages="errors" :label="label" :menu-props="menuProps" :readonly="readonly" v-on="$listeners">
+      <slot v-for="slotKey in slotKeys" :slot="slotKey" :name="slotKey" />
+      <template v-for="scopedSlotKey in scopedSlotKeys" :slot="scopedSlotKey" slot-scope="scope">
+        <slot v-bind="scope" :name="scopedSlotKey" />
+      </template>
+    </v-autocomplete>
+  </validation-provider>
+</template>
