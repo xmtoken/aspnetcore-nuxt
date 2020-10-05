ece8445fcd5fd1b85d6714320f3e10a792d9ce73
xm <xmid1001@gmail.com>
2020-10-05 23:20:13 +0900

    add AppScope component

---
+++ src/Applications.WebHost/ClientApp/components/AppScope/AppScope.vue
@@ -0,0 +1,10 @@
+<script lang="ts">
+import Vue, { VNode } from 'vue';
+
+export default Vue.extend({
+  inheritAttrs: false,
+  render(): VNode {
+    return this.$slots.default![0];
+  },
+});
+</script>
