d4b10459bfb7d7cbedafe2d82d15f2fd73d93137
xm <xmid1001@gmail.com>
2020-10-06 23:03:37 +0900

    refactoring vee-validation birthday validator

--- src/Applications.WebHost/ClientApp/plugins/validation-rules/birthday.ts
+++ src/Applications.WebHost/ClientApp/plugins/validation-rules/birthday.ts
@@ -6,7 +6,7 @@ const rule: ValidationRule = {
   message: '{_field_}は過去の日付を入力してください。',
   validate(val: string | null | undefined): boolean {
     const value = DateFormatter.pretty(val);
-    return DateFormatter.isValid(value) && dayjs(value).format('YYYYMMDD') < dayjs().format('YYYYMMDD');
+    return DateFormatter.isValid(value) && dayjs(value).isBefore(dayjs(), 'day');
   },
 };
 
