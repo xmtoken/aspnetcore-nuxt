4ff822d67e5c32018796dcbaba81f8258c3a22c0
xm <xmid1001@gmail.com>
2020-11-03 00:30:36 +0900

    AppCheckbox supports mandatory prop

--- src/Applications.WebHost/ClientApp/components/AppCheckbox/AppCheckbox.vue
+++ src/Applications.WebHost/ClientApp/components/AppCheckbox/AppCheckbox.vue
@@ -1,5 +1,6 @@
 <script lang="ts">
 import { ValidationProvider } from 'vee-validate';
+import { PropType } from 'vue';
 import mixins from '~/extensions/mixins';
 import requiredMarkable from '~/mixins/required-markable';
 import slotable from '~/mixins/slotable';
@@ -14,12 +15,45 @@ export default mixins(requiredMarkable, slotable, validationProviderProps).exten
     event: 'change',
     prop: 'inputValue',
   },
+  props: {
+    falseValue: {
+      default: false,
+      type: (null as any) as PropType<any>,
+    },
+    inputValue: {
+      default: undefined,
+      type: (null as any) as PropType<any>,
+    },
+    mandatory: {
+      default: true,
+      type: Boolean,
+    },
+    trueValue: {
+      default: true,
+      type: (null as any) as PropType<any>,
+    },
+  },
+  watch: {
+    inputValue(): void {
+      this.updateMandatory();
+    },
+  },
+  created(): void {
+    this.updateMandatory();
+  },
+  methods: {
+    updateMandatory(): void {
+      if (this.mandatory && this.inputValue !== this.falseValue && this.inputValue !== this.trueValue) {
+        this.$emit('change', this.falseValue);
+      }
+    },
+  },
 });
 </script>
 
 <template>
   <validation-provider v-slot="{ errors, required }" v-bind="veeProviderProps">
-    <v-checkbox v-bind="$attrs" :class="{ required, 'required-marker': required && !disabledRequiredMarker }" :disabled="disabled" :error-messages="errors" :label="label" :readonly="readonly" v-on="$listeners">
+    <v-checkbox v-bind="$attrs" :class="{ required, 'required-marker': required && !disabledRequiredMarker }" :disabled="disabled" :error-messages="errors" :false-value="falseValue" :label="label" :readonly="readonly" :true-value="trueValue" v-on="$listeners">
       <slot v-for="slotKey in slotKeys" :slot="slotKey" :name="slotKey" />
       <template v-for="scopedSlotKey in scopedSlotKeys" :slot="scopedSlotKey" slot-scope="scope">
         <slot v-bind="scope" :name="scopedSlotKey" />
